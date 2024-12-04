55474a1ddf0bce0d5693c5e1b1dc00543dd6ed62
Chris Fulara <fularac@umich.edu>
2024-12-03 22:53:49 -0500

    Revert to day2 part 1 solution

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -14,11 +14,11 @@ fn main() {
             .split_whitespace()
             .map(|s| s.parse::<isize>().unwrap())
             .collect();
-        // if still_safe(temps[0], &temps[1..], None, true) {
-        //     //println!("{} is safe!", line);
-        //     safe_count+=1;
-        // }
-        if still_safe(temps[0], &temps[1..], None, false) {
+        if still_safe(temps[0], &temps[1..], None) {
+            //println!("{} is safe!", line);
+            safe_count += 1;
+        }
+        if still_safe_part2(temps[0], &temps[1..], None, false) {
             println!("{} is safe (part2)!", line);
             safe_count2 += 1;
         } else {
@@ -29,35 +29,24 @@ fn main() {
     println!("Part 2 total safe: {}", safe_count2);
 }
 
-fn still_safe(
-    last_temp: isize,
-    remaining: &[isize],
-    assending: Option<bool>,
-    already_failed: bool,
-) -> bool {
+fn still_safe(last_temp: isize, remaining: &[isize], assending: Option<bool>) -> bool {
     if remaining.is_empty() {
         return true;
     }
     let new_temp = remaining[0];
-    let diff = new_temp -  last_temp;
+    let diff = new_temp - last_temp;
     if diff.abs() == 0 || diff.abs() > 3 {
         println!(
             "Unsafe! {} -> {}: diff({}) 0 or >3 ",
             last_temp, new_temp, diff
         );
-        return match already_failed {
-            false =>  still_safe(last_temp, &remaining[1..], assending, true) 
-            || assending.is_none() && still_safe(new_temp, &remaining[1..], assending, true),
-            true => false,
-        };
+        return false;
     }
     if assending.is_none() {
-        return still_safe(new_temp, &remaining[1..], Some(diff > 0), already_failed)
-            || !already_failed && still_safe(last_temp, &remaining[1..], None, true);
+        return still_safe(new_temp, &remaining[1..], Some(diff > 0));
     } else {
         if assending.unwrap() && diff > 0 || !assending.unwrap() && diff < 0 {
-            return still_safe(new_temp, &remaining[1..], assending, already_failed) ||
-            still_safe(last_temp, &remaining[1..], assending, true);
+            return still_safe(new_temp, &remaining[1..], assending);
         } else {
             let direction = if assending.unwrap() {
                 "assending"
@@ -68,15 +57,19 @@ fn still_safe(
                 "Unsafe! {} -> {}: change in temperature direction to {}",
                 last_temp, new_temp, direction
             );
-            if already_failed {
-                return false;
-            } else {
-              return still_safe(last_temp, &remaining[1..], assending, true);
-            }
+            return false;
         }
     }
 }
 
+fn still_safe_part2(
+    last_temp: isize,
+    remaining: &[isize],
+    assending: Option<bool>,
+    already_failed: bool,
+) -> bool {
+    false
+}
 #[cfg(test)]
 mod test {
     use super::*;
@@ -84,31 +77,35 @@ mod test {
     #[test]
     fn safe_part1() {
         let lines: Vec<Vec<isize>> = "7 6 4 2 1
-1 3 6 7 9".split_terminator("\n")
-.map(|s| {
-     s.split_whitespace().map(|line| line.parse::<isize>().unwrap()).collect()
-    })
-.collect();
-    for line in lines {
-        println!("Testing: {:?}", line);
-        assert!(still_safe(line[0], &line[1..], None, true))
-    }
-
+1 3 6 7 9"
+            .split_terminator("\n")
+            .map(|s| {
+                s.split_whitespace()
+                    .map(|line| line.parse::<isize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        for line in lines {
+            println!("Testing: {:?}", line);
+            assert!(still_safe(line[0], &line[1..], None))
+        }
     }
     #[test]
     fn unsafe_part1() {
         let lines: Vec<Vec<isize>> = "1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
-8 6 4 4 1".split_terminator("\n")
-.map(|s| {
-     s.split_whitespace().map(|line| line.parse::<isize>().unwrap()).collect()
-    })
-.collect();
-    for line in lines {
-        println!("Testing: {:?}", line);
-        assert!(!still_safe(line[0], &line[1..], None, true))
+8 6 4 4 1"
+            .split_terminator("\n")
+            .map(|s| {
+                s.split_whitespace()
+                    .map(|line| line.parse::<isize>().unwrap())
+                    .collect()
+            })
+            .collect();
+        for line in lines {
+            println!("Testing: {:?}", line);
+            assert!(!still_safe(line[0], &line[1..], None))
+        }
     }
-    }
-
 }
